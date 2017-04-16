0758906cb56c614693c92715959bbd23a6075050
spasarok <spasarok@grinnell.edu>
2017-04-16 16:01:53 -0500

    Update archive.php

--- archive.php
+++ archive.php
@@ -2,7 +2,7 @@
 
 <main class="archive">
     <div class="container skinny">
-        <h1>Lessons</h1>
+        <h1><?php the_archive_title(); ?></h1>
 
         <?php while(have_posts()): the_post(); ?>
             <article>
