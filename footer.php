26c4828e307b4ecc32ee585f97d2ddc564e77d4e
Kim Spasaro <spasarok@grinnell.edu>
2017-07-08 08:31:09 -0500

    updating footer width

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <?php wp_footer(); ?>
-    <div class="container">
+    <div class="container skinny">
         <p>This site and its intellectual contents copyright Kim Spasaro <?php echo date('Y'); ?>.</p>
         <p class="hidden">Images requiring citation link to their source.</p>
         <p>Site icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> and are licensed under <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
